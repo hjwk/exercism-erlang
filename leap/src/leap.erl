69b72e153f7c1177be8fabcac6e974fecbc777e7
Hubert Woszczyk <woszczykh@gmail.com>
2020-12-24 12:25:45 +0100

    Better leap solution
    Replaced if else by boolean expression

--- leap/src/leap.erl
+++ leap/src/leap.erl
@@ -3,14 +3,5 @@
 -export([leap_year/1]).
 
 
-leap_year(Year) -> 
-    if
-        Year rem 400 =:= 0 ->
-            true;
-        Year rem 100 =:= 0 ->
-            false;
-        Year rem 4 =:=0 ->
-            true;
-        true ->
-            false
-    end.
+leap_year(Year) ->
+    ((Year rem 4 =:=0) and not (Year rem 100 =:= 0)) or (Year rem 400 =:= 0).
